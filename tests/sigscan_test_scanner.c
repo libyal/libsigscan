fdaef16a4de3feeda7ba04738e15087870b5dd64
Joachim Metz <joachim.metz@gmail.com>
2014-12-28 15:42:00 +0100

    Worked on tests.

---
+++ tests/sigscan_test_scanner.c
@@ -0,0 +1,179 @@
+/*
+ * Library scanner type testing program
+ *
+ * Copyright (c) 2014, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <common.h>
+
+#if defined( HAVE_STDLIB_H ) || defined( WINAPI )
+#include <stdlib.h>
+#endif
+
+#include <stdio.h>
+
+#include "sigscan_test_libcerror.h"
+#include "sigscan_test_libcstring.h"
+#include "sigscan_test_libsigscan.h"
+#include "sigscan_test_unused.h"
+
+/* Tests initializing the scanner
+ * Make sure the value scanner is referencing, is set to NULL
+ * Returns 1 if successful, 0 if not or -1 on error
+ */
+int sigscan_test_scanner_initialize(
+     libsigscan_scanner_t **scanner,
+     int expected_result )
+{
+	libcerror_error_t *error = NULL;
+	static char *function    = "sigscan_test_scanner_initialize";
+	int result               = 0;
+
+	fprintf(
+	 stdout,
+	 "Testing initialize\t" );
+
+	result = libsigscan_scanner_initialize(
+	          scanner,
+	          &error );
+
+	if( result == -1 )
+	{
+		libcerror_error_set(
+		 &error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_INITIALIZE_FAILED,
+		 "%s: unable to create scanner.",
+		 function );
+	}
+	if( result != expected_result )
+	{
+		fprintf(
+		 stdout,
+		 "(FAIL)" );
+	}
+	else
+	{
+		fprintf(
+		 stdout,
+		 "(PASS)" );
+	}
+	fprintf(
+	 stdout,
+	 "\n" );
+
+	if( result == -1 )
+	{
+		libcerror_error_backtrace_fprint(
+		 error,
+		 stdout );
+
+		libcerror_error_free(
+		 &error );
+	}
+	if( result == 1 )
+	{
+		if( libsigscan_scanner_free(
+		     scanner,
+		     &error ) == -1 )
+		{
+			libcerror_error_set(
+			 &error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_FINALIZE_FAILED,
+			 "%s: unable to free scanner.",
+			 function );
+
+			libcerror_error_backtrace_fprint(
+			 error,
+			 stdout );
+
+			libcerror_error_free(
+			 &error );
+
+			return( -1 );
+		}
+	}
+	if( result != expected_result )
+	{
+		return( 0 );
+	}
+	return( 1 );
+}
+
+/* The main program
+ */
+#if defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
+int wmain( int argc, wchar_t * const argv[] SIGSCAN_TEST_ATTRIBUTE_UNUSED )
+#else
+int main( int argc, char * const argv[] SIGSCAN_TEST_ATTRIBUTE_UNUSED )
+#endif
+{
+	libsigscan_scanner_t *scanner = NULL;
+
+	SIGSCAN_TEST_UNREFERENCED_PARAMETER( argv )
+
+	if( argc != 1 )
+	{
+		fprintf(
+		 stderr,
+		 "Unsupported number of arguments.\n" );
+
+		return( EXIT_FAILURE );
+	}
+	/* Initialization tests
+	 */
+	scanner = NULL;
+
+	if( sigscan_test_scanner_initialize(
+	     &scanner,
+	     1 ) != 1 )
+	{
+		fprintf(
+		 stderr,
+		 "Unable to test initialize.\n" );
+
+		return( EXIT_FAILURE );
+	}
+	scanner = NULL;
+
+	scanner = (libsigscan_scanner_t *) 0x12345678UL;
+
+	if( sigscan_test_scanner_initialize(
+	     &scanner,
+	     -1 ) != 1 )
+	{
+		fprintf(
+		 stderr,
+		 "Unable to test initialize.\n" );
+
+		return( EXIT_FAILURE );
+	}
+	if( sigscan_test_scanner_initialize(
+	     NULL,
+	     -1 ) != 1 )
+	{
+		fprintf(
+		 stderr,
+		 "Unable to test initialize.\n" );
+
+		return( EXIT_FAILURE );
+	}
+	return( EXIT_SUCCESS );
+}
+
---
+++ tests/test_scanner.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Library scanner testing script
+#
+# Copyright (c) 2014, Joachim Metz <joachim.metz@gmail.com>
+#
+# Refer to AUTHORS for acknowledgements.
+#
+# This software is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+EXIT_SUCCESS=0;
+EXIT_FAILURE=1;
+EXIT_IGNORE=77;
+
+test_scanner()
+{ 
+	echo "Testing scanner type";
+
+	./${SIGSCAN_TEST_SCANNER};
+
+	RESULT=$?;
+
+	echo "";
+
+	return ${RESULT};
+}
+
+SIGSCAN_TEST_SCANNER="sigscan_test_scanner";
+
+if ! test -x ${SIGSCAN_TEST_SCANNER};
+then
+	SIGSCAN_TEST_SCANNER="sigscan_test_scanner.exe";
+fi
+
+if ! test -x ${SIGSCAN_TEST_SCANNER};
+then
+	echo "Missing executable: ${SIGSCAN_TEST_SCANNER}";
+
+	exit ${EXIT_FAILURE};
+fi
+
+if ! test_scanner;
+then
+	exit ${EXIT_FAILURE};
+fi
+
+exit ${EXIT_SUCCESS};
+
