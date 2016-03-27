746fb2ed5420e2e469206079699a26cdf171a24f
Joachim Metz <joachim.metz@gmail.com>
2016-03-27 09:55:48 +0200

    Worked on tests.

--- tests/test_scanner.sh
+++ tests/test_scanner.sh
@@ -1,74 +1,79 @@
 #!/bin/bash
 # Library scanner type testing script
 #
-# Version: 20160128
+# Version: 20160326
 
 EXIT_SUCCESS=0;
 EXIT_FAILURE=1;
 EXIT_IGNORE=77;
 
-TEST_PREFIX=`pwd`;
-TEST_PREFIX=`dirname ${TEST_PREFIX}`;
-TEST_PREFIX=`basename ${TEST_PREFIX} | sed 's/^lib//'`;
+TEST_PREFIX=`dirname ${PWD}`;
+TEST_PREFIX=`basename ${TEST_PREFIX} | sed 's/^lib\([^-]*\)/\1/'`;
+
+TEST_PROFILE="lib${TEST_PREFIX}";
+TEST_DESCRIPTION="scanner type";
+OPTION_SETS="";
+
+TEST_TOOL_DIRECTORY=".";
+TEST_TOOL="${TEST_PREFIX}_test_scanner";
+
+test_scanner_type()
+{
+	local TEST_EXECUTABLE=$1;
+
+	echo -n -e "Testing scanner\t"
+
+	run_test_with_arguments ${TEST_EXECUTABLE};
+	local RESULT=$?;
+
+	if test ${RESULT} -ne 0;
+	then
+		echo "(FAIL)";
+	else
+		echo "(PASS)";
+	fi
+	echo "";
+
+	return ${RESULT};
+}
 
 if ! test -z ${SKIP_LIBRARY_TESTS};
 then
 	exit ${EXIT_IGNORE};
 fi
 
-TEST_SCANNER="./${TEST_PREFIX}_test_scanner";
+TEST_EXECUTABLE="${TEST_TOOL_DIRECTORY}/${TEST_TOOL}";
 
-if ! test -x ${TEST_SCANNER};
+if ! test -x "${TEST_EXECUTABLE}";
 then
-	TEST_SCANNER="${TEST_PREFIX}_test_scanner.exe";
+	TEST_EXECUTABLE="${TEST_TOOL_DIRECTORY}/${TEST_TOOL}.exe";
 fi
 
-if ! test -x ${TEST_SCANNER};
+if ! test -x "${TEST_EXECUTABLE}";
 then
-	echo "Missing executable: ${TEST_SCANNER}";
+	echo "Missing test executable: ${TEST_EXECUTABLE}";
 
 	exit ${EXIT_FAILURE};
 fi
 
 TEST_RUNNER="tests/test_runner.sh";
 
-if ! test -x ${TEST_RUNNER};
+if ! test -f "${TEST_RUNNER}";
 then
 	TEST_RUNNER="./test_runner.sh";
 fi
 
-if ! test -x ${TEST_RUNNER};
+if ! test -f "${TEST_RUNNER}";
 then
 	echo "Missing test runner: ${TEST_RUNNER}";
 
 	exit ${EXIT_FAILURE};
 fi
 
-echo -n -e "Testing scanner\t"
+source ${TEST_RUNNER};
 
-TMPDIR="tmp$$";
-
-rm -rf ${TMPDIR};
-mkdir ${TMPDIR};
-
-${TEST_RUNNER} ${TMPDIR} ${TEST_SCANNER};
-
+test_scanner_type "${TEST_EXECUTABLE}";
 RESULT=$?;
 
-rm -rf ${TMPDIR};
-
-if test ${RESULT} -ne 0;
-then
-	echo "(FAIL)";
-else
-	echo "(PASS)";
-fi
-echo "";
-
-if test ${RESULT} -ne 0;
-then
-	exit ${EXIT_FAILURE};
-fi
-
-exit ${EXIT_SUCCESS};
+exit ${RESULT};
 
