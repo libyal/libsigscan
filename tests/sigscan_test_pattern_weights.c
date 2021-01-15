71702aa9d6b799e367bbc5bfb4a9c6d445771794
Joachim Metz <joachim.metz@gmail.com>
2021-01-15 07:22:06 +0100

    Applied updates

--- tests/sigscan_test_pattern_weights.c
+++ tests/sigscan_test_pattern_weights.c
@@ -1,7 +1,7 @@
 /*
  * Library pattern_weights type test program
  *
- * Copyright (C) 2014-2020, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2014-2021, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
