5edf9bbf4f951e91c7632282376b5085a3e11341
Joachim Metz <joachim.metz@gmail.com>
2023-11-17 05:10:25 +0100

    Applied updates

--- sigscantools/sigscantools_signal.c
+++ sigscantools/sigscantools_signal.c
@@ -40,7 +40,7 @@ void (*sigscantools_signal_signal_handler)( sigscantools_signal_t ) = NULL;
 /* Signal handler for Ctrl+C or Ctrl+Break signals
  */
 BOOL WINAPI sigscantools_signal_handler(
-             unsigned long signal )
+             sigscantools_signal_t signal )
 {
 	static char *function = "sigscantools_signal_handler";
 
@@ -112,7 +112,7 @@ int sigscantools_signal_attach(
 	sigscantools_signal_signal_handler = signal_handler;
 
 	if( SetConsoleCtrlHandler(
-	     sigscantools_signal_handler,
+	     (PHANDLER_ROUTINE) sigscantools_signal_handler,
 	     TRUE ) == 0 )
 	{
 		libcerror_error_set(
@@ -179,7 +179,7 @@ int sigscantools_signal_detach(
 	static char *function = "sigscantools_signal_detach";
 
 	if( SetConsoleCtrlHandler(
-	     sigscantools_signal_handler,
+	     (PHANDLER_ROUTINE) sigscantools_signal_handler,
 	     FALSE ) == 0 )
 	{
 		libcerror_error_set(
