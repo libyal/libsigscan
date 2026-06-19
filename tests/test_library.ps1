4654a8006bb4b8e30005ce8764c8fb9b58875289
Joachim Metz <joachim.metz@gmail.com>
2026-06-19 10:37:19 +0200

    Worked on tests and changes for CI tests

--- tests/test_library.ps1
+++ tests/test_library.ps1
@@ -1,71 +1,10 @@
 # Tests library functions and types.
-#
-# Version: 20260615
 
 $LibraryTests = "byte_value_group error identifier notify offset_group offsets_list pattern_weights scan_object scan_result scan_state scan_tree scan_tree_node signature signature_group signature_table signatures_list skip_table support weight_group"
 $LibraryTestsWithInput = "scanner"
 $OptionSets = "" -split " "
 
 . .\test_functions.ps1
-
-Function RunTest
-{
-	param( [string]$TestName )
-
-	$TestBinary = "sigscan_test_${TestName}"
-
-	$TestDescription = "${TestBinary}"
-	$TestExecutable = "${TestExecutablesDirectory}\${TestBinary}.exe"
-
-	If (-Not (Test-Path -Path ${TestExecutable} -PathType Leaf))
-	{
-		WriteTestResult ${TestDescription} ${ExitIgnore}
-
-		Return ${ExitIgnore}
-	}
-	$Output = Invoke-Expression ${TestExecutable}
-	$Result = $global:LastExitCode
-
-	If (${Result} -ne ${ExitSuccess})
-	{
-		Write-Host ${Output} -foreground Red
-	}
-	WriteTestResult ${TestDescription} ${Result}
-
-	Return ${Result}
-}
-
-Function RunTestWithInput
-{
-	param( [string]$TestName, [string[]]$TestInput )
-
-	$OptionSet = $TestInput[0]
-	$Options = $TestInput[1]
-	$TestFile = $TestInput[2]
-
-	$TestBinary = "sigscan_test_${TestName}"
-	$TestFileName = (${TestFile} -split '\\')[-2..-1] -join '\'
-
-	$TestDescription = "${TestBinary} with input: '${TestFileName}"
-	$TestExecutable = "${TestExecutablesDirectory}\${TestBinary}.exe"
-
-	If (-Not (Test-Path -Path ${TestExecutable} -PathType Leaf))
-	{
-		WriteTestResult ${TestDescription} ${ExitIgnore}
-
-		Return ${ExitIgnore}
-	}
-	$Output = Invoke-Expression "${TestExecutable} ${Options} ${TestFile}"
-	$Result = $global:LastExitCode
-
-	If (${Result} -ne ${ExitSuccess})
-	{
-		Write-Host ${Output} -foreground Red
-	}
-	WriteTestResult ${TestDescription} ${Result}
-
-	Return ${Result}
-}
 
 $TestExecutablesDirectory = GetTestExecutablesDirectory
 
@@ -85,7 +24,7 @@ Foreach (${TestName} in ${LibraryTests} -split " ")
 	{
 		Continue
 	}
-	$Result = RunTest ${TestName}
+	$Result = RunTestBinary ${TestExecutablesDirectory} "sigscan_test_${TestName}"
 
 	If ((${Result} -ne ${ExitSuccess}) -And (${Result} -ne ${ExitIgnore}))
 	{
@@ -104,7 +43,7 @@ Foreach (${TestName} in ${LibraryTestsWithInput} -split " ")
 	}
 	ForEach ($TestInput in ${TestInputs})
 	{
-		$Result = RunTestWithInput ${TestName} ${TestInput}
+		$Result = RunTestBinaryWithInput ${TestExecutablesDirectory} "sigscan_test_${TestName}" ${TestInput}
 
 		If ((${Result} -ne ${ExitSuccess}) -And (${Result} -ne ${ExitIgnore}))
 		{
---
+++ tests/test_tools.ps1
@@ -0,0 +1,19 @@
+# Tests tools functions and types.
+
+$ToolsTests = ""
+$OptionSets = "" -split " "
+
+. .\test_functions.ps1
+
+$TestExecutablesDirectory = GetTestExecutablesDirectory
+
+If (-Not (Test-Path ${TestExecutablesDirectory}))
+{
+	Write-Error "Missing test executables directory"
+
+	Exit ${ExitFailure}
+}
+
+$Result = ${ExitIgnore}
+
+Exit ${Result}
